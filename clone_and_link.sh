75ae44442b172527cfbd01f505866b56f5886dff
Philip Nelson <philipgnelson@me.com>
2013-02-18 20:19:17 -0600

    First commit

---
+++ clone_and_link.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd &&
+[ -d '.dot-files' ] || git clone git://github.com/almostthor/dotfiles.git .dotfiles &&
+ls -1d .dotfiles/files/* .dotfiles/files/.* | while read f; do
+  [ "$f" == '.dotfiles/files/.' ] ||
+  [ "$f" == '.dotfiles/files/..' ] ||
+  [ "$f" == '.dotfiles/files/.git' ] ||
+  ln -vsf "$f" .
+done
---
+++ files/.zshrc
@@ -0,0 +1,5 @@
+. ~/.zsh/rc
+
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
