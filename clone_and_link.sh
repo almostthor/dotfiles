264a9f0c07b438229dd1c2cb3f612c6365c9099a
Philip Nelson <philipgnelson@me.com>
2013-12-15 21:35:24 -0600

    Update clone_and_link to ignore Library settings

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -6,5 +6,6 @@ ls -1d .dotfiles/files/* .dotfiles/files/.* | while read f; do
   [ "$f" == '.dotfiles/files/.' ] ||
   [ "$f" == '.dotfiles/files/..' ] ||
   [ "$f" == '.dotfiles/files/.git' ] ||
+  [ "$f" == '.dotfiles/files/Library' ] ||
   ln -vsf "$f" .
 done
